620278c6d4492f84ad7f5b9a86a4b9a1e0fb43a6
pvtodorov <petar.v.todorov@gmail.com>
2022-10-14 21:24:19 +0200

    probe_selection prep targets and functions

--- probe_selection/_targets.R
+++ probe_selection/_targets.R
@@ -14,8 +14,7 @@ library(tidyr)
 # Set target options:
 tar_option_set(
 #   packages = c("tibble"), # packages that your targets need to run
-  format = "qs", # default storage format,
-  error = "continue",
+  format = "qs" # default storage format,
   # Set other options as needed.
 )
 
@@ -26,12 +25,13 @@ source(paste0("prep_probe_selection.R"))
 # tar_make_clustermq() configuration (okay to leave alone):
 options(clustermq.scheduler = "multicore")
 options(future.globals.maxSize= (200 * 1000)*1024^2) #200GB
+Sys.setenv(BASILISK_EXTERNAL_DIR = paste0(Sys.getenv("PROJECT_DIR"), '/.basilisk'))
 
 # tar_make_future() configuration (okay to leave alone):
 # Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
 
 # Run the R scripts in the R/ folder with your custom functions:
-tar_source()
+# tar_source()
 # source("other_functions.R") # Source other scripts as needed. # nolint
 
 # Replace the target list below with your own:
@@ -42,8 +42,7 @@ list(
   tar_target(exp_labelled_other, qs::qread(exp_labelled_other_path)),
   tar_target(exp_labelled_neuron, qs::qread(exp_labelled_neuron_path)),
   tar_target(pldf_obob5v5, 
-             qs::qread(pldf_obob5v5_path) %>%
-             prep_pldf),
+             qs::qread(pldf_obob5v5_path)),
   tar_target(exp_labelled_other_ngo,
              exp_labelled_other %>%
                prep_obj(pldf_obob5v5) %>%
@@ -58,5 +57,18 @@ list(
              merge_neuron_and_other(exp_labelled_neuron_ngo, exp_labelled_other_ngo) %>%
              sc_transform_fgf1),
   tar_target(sce_ngo,
-             convert_obj_to_sce(exp_ngo))
+             convert_obj_to_sce(exp_ngo)),
+  # tar_target(basilisk_env,
+  #            make_basilisk_env()),
+  # tar_target(basilisk_roundtrip,
+  #            run_basilisk_convert(sce_ngo, '/scratch/nmq407/sce_ngo_h5ad', basilisk_env))
+  tar_target(sct_var_feats_10k,
+             get_sct_variable_features(exp_ngo, 10000)),
+  tar_target(exp_labelled_other_ngo_10kf,
+             subset_seurat_obj_genes(exp_labelled_other_ngo, sct_var_feats_10k) %>%
+             sc_transform_campbell %>% run_sct_chaser),
+  tar_target(exp_labelled_neuron_ngo_10kf,
+             subset_seurat_obj_genes(exp_labelled_neuron_ngo, sct_var_feats_10k) %>%
+             sc_transform_campbell %>% run_sct_chaser)
+             #####working here to make objects that wont change in nfeats
 )
--- probe_selection/prep_probe_selection.R
+++ probe_selection/prep_probe_selection.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(Seurat)
+library(zellkonverter)
+library(basilisk)
+library(scRNAseq)
 
 prep_pldf = function(pldf){
     pldf = pldf %>% 
@@ -16,4 +19,39 @@ prep_obj = function(obj, pldf){
     obj@meta.data = obj@meta.data %>%
       mutate(polar_label = ifelse(is.na(polar_label), stringr::str_replace(labels, '-', '__') %>% paste0('.none'), polar_label))
     obj
+}
+
+
+make_basilisk_env = function(){
+    bask_env = zellkonverter::zellkonverterAnnDataEnv()
+    bask_env
+}
+
+
+run_basilisk_convert = function(sce, path, basilisk_env){
+    roundtrip = basiliskRun(fun = function(sce) {
+        # Convert SCE to AnnData:
+    #      adata <- SCE2AnnData(sce)
+
+        # Maybe do some work in Python on 'adata':
+        # BLAH BLAH BLAH
+
+        # Convert back to an SCE:
+    #     writeH5AD(sce, '/scratch/nmq407/exp_04_labelled_h5ad')
+        writeH5AD(sce, path)
+    }, env = basilisk_env, sce = sce)
+    roundtrip
+} 
+
+
+get_sct_variable_features = function(obj, nfeats){
+    obj <- Seurat::SCTransform(obj ,
+                        assay='RNA',
+                        method="glmGamPoi",
+                        vars.to.regress="batch",
+                        vst.flavor="v2",
+                        variable.features.n = nfeats,          
+                        verbose=TRUE)
+    var_feats = obj@assays$SCT@var.features
+    var_feats
 }
