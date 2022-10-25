d63c8322d2ef8b845033b45bf11cfdcdb1a00537
pvtodorov <petar.v.todorov@gmail.com>
2022-10-25 16:32:15 +0200

    gbr get values of not named genes from vector

--- genebasisr/genebasisr.R
+++ genebasisr/genebasisr.R
@@ -11,7 +11,16 @@ gbr_retain_informative_genes = function(sce, n=NULL){
     sce
 }
 
-gbr_genes_stat = function(sce, n_genes){
+get_not_named_genes_vec = function(vec_of_genes){
+    a_rik = vec_of_genes %>% stringr::str_ends("Rik")
+    an_ensmusg = vec_of_genes %>% stringr::str_starts("ENSMUSG")
+    a_Gm = vec_of_genes %>% stringr::str_starts("Gm\\d")
+    starts_w4_or_more_digits = vec_of_genes %>% stringr::str_ends("\\d{4,}")
+    not_named_genes = (a_rik | an_ensmusg | a_Gm | starts_w4_or_more_digits) %>% which
+    vec_of_genes[not_named_genes]
+}
+
+
     genes_stat = geneBasisR::gene_search(sce,
                              n_genes_total = n_genes,
                              verbose = T)
